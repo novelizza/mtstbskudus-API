eda2c06b41ff5458731d779b02e06b262bcd48e7
novelizza <novel.izza.fardana@gmail.com>
2023-02-24 12:45:31 +0700

    git ignore

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("mtstbs", "admin_database", "AdminMtsTbsKudus2023=", {
+const db = new Sequelize("tbs", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
