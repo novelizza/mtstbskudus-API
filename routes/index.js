0aff1453fa7378578eea4b8a239b61f9a6a0f453
novelizza <novel.izza.fardana@gmail.com>
2023-03-05 11:20:36 +0700

    add path image

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ indexRouter.get("/", (req, res) => {
   res.send("Wellcome to TBS API!");
 });
 
+indexRouter.use("/avatar", express.static("image/siswa"));
+indexRouter.use("/galeri", express.static("image/galeri"));
+indexRouter.use("/brosur", express.static("image/brosur"));
+indexRouter.use("/banner", express.static("image/banner"));
+
 indexRouter.use("/auth", authRoute);
 indexRouter.use("/siswa", siswaRoute);
 indexRouter.use("/admin", adminRoute);
