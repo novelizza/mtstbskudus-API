88601c8e60447b0462278ced5404080a7be91d16
novelizza <novel.izza.fardana@gmail.com>
2023-03-09 07:59:44 +0700

    test create

--- controller/siswa.controller.js
+++ controller/siswa.controller.js
@@ -119,42 +119,45 @@ const postSiswa = async (req, res) => {
     .then(async (result) => {
       console.log(result.data);
 
-      const parsed_string = BniEnc.decrypt(result.data.data, CID, SCK);
+      setContent(200, result.data);
+      return res.status(200).json(getContent());
 
-      const akunSiswa = await akunSiswaModel.findOne({
-        where: {
-          username: req.body.username,
-        },
-      });
+      // const parsed_string = BniEnc.decrypt(result.data.data, CID, SCK);
 
-      if (!akunSiswa) {
-        if (req.file == undefined) {
-          setContent(201, "image upload failed.");
-          return res.status(201).json(getContent());
-        } else {
-          try {
-            const newSiswa = new akunSiswaModel(req.body);
-            newSiswa.password = crypto.MD5(req.body.password).toString();
-            newSiswa.avatar = req.file.filename;
-            newSiswa.tahun_masuk =
-              moment().year() + "/" + (Number(moment().year()) + 1);
-            newSiswa.bayar = 0;
-            newSiswa.va = parsed_string.virtual_account;
-            newSiswa.trx_id = parsed_string.trx_id;
+      // const akunSiswa = await akunSiswaModel.findOne({
+      //   where: {
+      //     username: req.body.username,
+      //   },
+      // });
 
-            await newSiswa.save();
+      // if (!akunSiswa) {
+      //   if (req.file == undefined) {
+      //     setContent(201, "image upload failed.");
+      //     return res.status(201).json(getContent());
+      //   } else {
+      //     try {
+      //       const newSiswa = new akunSiswaModel(req.body);
+      //       newSiswa.password = crypto.MD5(req.body.password).toString();
+      //       newSiswa.avatar = req.file.filename;
+      //       newSiswa.tahun_masuk =
+      //         moment().year() + "/" + (Number(moment().year()) + 1);
+      //       newSiswa.bayar = 0;
+      //       newSiswa.va = parsed_string.virtual_account;
+      //       newSiswa.trx_id = parsed_string.trx_id;
 
-            setContent(200, "Siswa Berhasil Ditambahkan");
-            return res.status(200).json(getContent());
-          } catch (error) {
-            setContent(500, error);
-            return res.status(500).json(getContent());
-          }
-        }
-      } else {
-        setContent(500, "Username Telah Terdaftar");
-        return res.status(500).json(getContent());
-      }
+      //       await newSiswa.save();
+
+      //       setContent(200, "Siswa Berhasil Ditambahkan");
+      //       return res.status(200).json(getContent());
+      //     } catch (error) {
+      //       setContent(500, error);
+      //       return res.status(500).json(getContent());
+      //     }
+      //   }
+      // } else {
+      //   setContent(500, "Username Telah Terdaftar");
+      //   return res.status(500).json(getContent());
+      // }
     })
     .catch((er) => {
       console.log("------------------------");
