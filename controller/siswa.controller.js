f73de1057a835e75edf8aa5507ff99628809f81d
novelizza <novel.izza.fardana@gmail.com>
2023-03-11 16:02:30 +0700

    make a prestasi

--- controller/siswa.controller.js
+++ controller/siswa.controller.js
@@ -581,7 +581,7 @@ const getDataPrestasi = async (req, res) => {
       // const p2 = getDataPrestasi.find((item2) => item2.prestasi_ke === "2");
       // const p3 = getDataPrestasi.find((item3) => item3.prestasi_ke === "3");
 
-      console.log(p1);
+      console.log(getDataPrestasi);
 
       setContent(200, {
         prestasi1: "p1",
